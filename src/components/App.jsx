6e3425888d7c23d4e09317b076bd8ef1f8782ddc
fdariia <fdariya3005@gmail.com>
2023-03-11 00:43:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,7 @@
 import { Component } from 'react';
-import css from './App.module.css';
+import '../index.css';
 import Seachbar from './Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-
-// const KEY = '34173138-558a9ee15c3093426fbf117d9';
-// const BASE_URL = 'https://pixabay.com/api';
-// componentDidMount() {
-//   fetch(
-//     `${BASE_URL}/?q=cat&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//   ).then(res => res.json().then(picture => this.setState({picture})));
-// }
 
 class App extends Component {
   state = {
@@ -22,7 +14,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className={css.app}>
+      <div className="App">
         <Seachbar onSubmit={this.handleFormSubmit} />
         <ImageGallery query={this.state.query} />
       </div>
