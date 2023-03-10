3c1092d5705adaedf87bc5d7f4e9be67bc926dd7
fdariia <fdariya3005@gmail.com>
2023-03-10 23:33:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import 'components/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App className="App" />
+    <App className="app" />
   </React.StrictMode>
 );
